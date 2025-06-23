23aa6e82b16d67b380187b678cc1f7d6b8d7f899
Farjana Eti <etimourakkhieti@gmail.com>
2025-06-23 15:38:10 +0600

    user created and get done

--- index.js
+++ index.js
@@ -24,6 +24,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("online-shopping").collection("users");
+
+  
+     //post sinUp users all info
+      app.post('/users', async (req, res) => {
+   const newuser=req.body;
+    const existingUser = await userCollection.findOne({ email :newuser.email});
+    if (existingUser) {
+        return res.status(400).json({ message: "User already exist" });
+    }
+    const result = await userCollection.insertOne(newuser);
+    res.json(result);
+});
+
+
+    //get users registered data
+     app.get('/users', async (req, res) => {
+      const email = req.query.email;
+      let result;
+      if(email){
+        const query = { email: email };
+        result = await userCollection.find(query).toArray();  
+      } 
+      else{
+     result=await userCollection.find().toArray();
+      }
+       res.send(result);
+    });
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
